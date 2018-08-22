af1e024deb207a439a53027354a8e3c016af6345
tiboth <bothtihamer@yahoo.com>
2018-08-22 17:11:58 +0300

    update

--- webApp/src/app/content/edit-user/edit-user.component.ts
+++ webApp/src/app/content/edit-user/edit-user.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {User} from '../../shared/user';
 import {ContentService} from '../shared/content.service';
 import {Role} from '../../shared/role';
+import {LangChangeEvent, TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-edit-user',
@@ -16,8 +17,18 @@ export class EditUserComponent implements OnInit {
   selectedItems = [];
   settings = {};
 
-  constructor(private contentService: ContentService) {
+  constructor(private contentService: ContentService, private translate: TranslateService) {
     this.model = new User();
+    this.translate.get('edit.selectroles').subscribe(res => {
+      this.settings['text'] = res;
+    });
+    this.translate.get('edit.selectall').subscribe(res => {
+      this.settings['selectAllText'] = res;
+    });
+    this.translate.get('edit.unselectall').subscribe(res => {
+      this.settings['unSelectAllText'] = res;
+      console.log(JSON.stringify(this.settings));
+    });
   }
 
   submitForm() {
@@ -25,34 +36,20 @@ export class EditUserComponent implements OnInit {
   }
 
   ngOnInit() {
-    // this.itemList = [
-    //   {'id': 1, 'itemName': 'India'},
-    //   {'id': 2, 'itemName': 'Singapore'},
-    //   {'id': 3, 'itemName': 'Australia'},
-    //   {'id': 4, 'itemName': 'Canada'},
-    //   {'id': 5, 'itemName': 'South Korea'},
-    //   {'id': 6, 'itemName': 'Brazil'}
-    // ];
-
     this.contentService.getAllRoles()
       .subscribe(roles => this.roles = roles,
         err => console.log(JSON.stringify(err)),
         () => {
-        this.roles.forEach(r => this.itemList.push({'id': r.id, 'itemName': r.roleType}));
+          this.roles.forEach(r => this.itemList.push({'id': r.id, 'itemName': r.roleType}));
         });
-
-    // this.selectedItems = [
-    //   {'id': 1, 'itemName': 'India'},
-    //   {'id': 2, 'itemName': 'Singapore'},
-    //   {'id': 3, 'itemName': 'Australia'},
-    //   {'id': 4, 'itemName': 'Canada'}];
-    this.settings = {
-      text: 'Select Roles',
-      selectAllText: 'Select All',
-      unSelectAllText: 'UnSelect All',
-      classes: 'myclass custom-class'
-    };
-
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.settings = {
+        text: this.translate.instant('edit.selectroles'),
+        selectAllText: this.translate.instant('edit.selectall'),
+        unSelectAllText: this.translate.instant('edit.unselectall'),
+        classes: 'myclass custom-class'
+      };
+    });
   }
 
 
